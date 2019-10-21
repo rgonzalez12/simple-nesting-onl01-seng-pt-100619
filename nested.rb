7bf7a19e6736da05a8cff33dd307bb6f06f388e7
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-21 06:55:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -52,10 +52,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
-     programmer_hash[:dennis_ritchie][:languages].to_s
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
